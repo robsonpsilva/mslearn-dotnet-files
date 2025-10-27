f8d29bc2c9ee8b1f1d062f1a70910375d715e3ea
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-10-27 10:10:07 -0300

    modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -2,4 +2,13 @@
 using System.IO;
 using System.Collections.Generic;
 
+IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("stores");
+
+foreach (var dir in listOfDirectories) {
+    Console.WriteLine(dir);
+}
+
+// Outputs:
+// stores/201
+// stores/202
 Console.WriteLine("Hello, World!");
